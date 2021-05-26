81ee4b951f6b8d28bc867a9632da5c63d0d68b5b
doramakh <anvarovna28@gmail.com>
2021-05-26 13:45:08 +0500

    add header section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import Header from './Components/Header'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <section>
-        Hello
-      </section>
+      <Header />
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,31 @@
 function Header () {
     return (
-        
-    )
+        <>
+            <header className="header">
+                <div className="container header-container">
+                    <a className="logo-link" href="#">
+                        <img src="./img/logo.png" alt="Apartment Teraphy Logo" width="240"/>
+                    </a>
+
+                    <div className="header-right">
+                        <a href="#">NEWSLETTERS</a>
+                        <a href="#"></a>
+                        <a className="profile-link" href="#">
+                            <img src="./img/profile.webp" alt="Profile img" width="64"/>
+                        </a>
+                        <label htmlFor="search">
+                        <input type="text" name="search" id="search"/>
+                        <img src="./img/search.png" alt="Search icon" width="32"/>
+                        </label>
+                        
+                    </div>
+                </div>
+                <nav className="nav">
+                    <div className="container"></div>
+                </nav>
+            </header>
+        </>
+    );
 }
+
+export default Header
