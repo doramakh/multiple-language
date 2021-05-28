048f3b5ae23f81127b953b8593ed888744c0d6e4
doramakh <anvarovna28@gmail.com>
2021-05-28 17:46:25 +0500

    add styles to hero page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './Components/Header'
+import Hero from './Components/Hero'
 
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,13 +1,13 @@
-function Hero {
+function Hero () {
     return (
         <>
             <section className="hero">
                 <div className="container">
                     <a className="article-link" href="#">
-                        <img className="hero-img" src="./hero-img.jpeg" alt="Article img"/>
-                        <span className="hero-category">TOUR</span>
+                        <img className="hero-img" src="./img/hero-img.jpeg" alt="Article img"/>
+                        <span className="hero-category">TOURS</span>
                         <h2 className="hero-title">9 Things You Learn Living in a Tiny Studio Apartment</h2>
-                        <div>
+                        <div className="hero-details">
                             <span className="hero-author">by Adrienne Breaux</span>
                             <span className="hero-time">Yesterday</span>
                         </div>
@@ -17,3 +17,5 @@ function Hero {
         </>
     )
 }
+
+export default Hero
