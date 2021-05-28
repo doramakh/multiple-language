88d461bbfbbf6a0ea2a2422725236c5668318d8b
doramakh <anvarovna28@gmail.com>
2021-05-28 17:35:42 +0500

    create hero section

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,19 @@
+function Hero {
+    return (
+        <>
+            <section className="hero">
+                <div className="container">
+                    <a className="article-link" href="#">
+                        <img className="hero-img" src="./hero-img.jpeg" alt="Article img"/>
+                        <span className="hero-category">TOUR</span>
+                        <h2 className="hero-title">9 Things You Learn Living in a Tiny Studio Apartment</h2>
+                        <div>
+                            <span className="hero-author">by Adrienne Breaux</span>
+                            <span className="hero-time">Yesterday</span>
+                        </div>
+                    </a>
+                </div>
+            </section>
+        </>
+    )
+}
